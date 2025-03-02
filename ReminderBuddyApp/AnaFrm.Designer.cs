a00fcfa58872269283c244e2e9e599422432ec86
Musta <Musta@MUSTAFASAHIN>
2025-03-02 13:21:41 +0300

    Add project files.

---
+++ ReminderBuddyApp/AnaFrm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ReminderBuddyApp
+{
+    partial class AnaFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            fluentDesignFormContainer1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer();
+            accordionControl1 = new DevExpress.XtraBars.Navigation.AccordionControl();
+            accordionControlElement1 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            fluentDesignFormControl1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl();
+            fluentFormDefaultManager1 = new DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager(components);
+            accordionControlElementKategori = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElementAnimsatici = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElementNotlar = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElementHedefler = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElementKisaVadeliHedefler = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElementOrtaVadeHedefeler = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElementUzunVadeHedefler = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElementHakkımda = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            ((System.ComponentModel.ISupportInitialize)accordionControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)fluentDesignFormControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)fluentFormDefaultManager1).BeginInit();
+            SuspendLayout();
+            // 
+            // fluentDesignFormContainer1
+            // 
+            fluentDesignFormContainer1.Dock = DockStyle.Fill;
+            fluentDesignFormContainer1.Font = new Font("Corbel", 13.875F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            fluentDesignFormContainer1.Location = new Point(414, 62);
+            fluentDesignFormContainer1.Name = "fluentDesignFormContainer1";
+            fluentDesignFormContainer1.Size = new Size(1383, 990);
+            fluentDesignFormContainer1.TabIndex = 0;
+            // 
+            // accordionControl1
+            // 
+            accordionControl1.Dock = DockStyle.Left;
+            accordionControl1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement1, accordionControlElementKategori, accordionControlElementHedefler });
+            accordionControl1.Location = new Point(0, 62);
+            accordionControl1.Name = "accordionControl1";
+            accordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Touch;
+            accordionControl1.Size = new Size(414, 990);
+            accordionControl1.TabIndex = 1;
+            accordionControl1.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu;
+            // 
+            // accordionControlElement1
+            // 
+            accordionControlElement1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElementHakkımda });
+            accordionControlElement1.Expanded = true;
+            accordionControlElement1.Name = "accordionControlElement1";
+            accordionControlElement1.Text = "Mustafa Şahin";
+            // 
+            // fluentDesignFormControl1
+            // 
+            fluentDesignFormControl1.FluentDesignForm = this;
+            fluentDesignFormControl1.Location = new Point(0, 0);
+            fluentDesignFormControl1.Manager = fluentFormDefaultManager1;
+            fluentDesignFormControl1.Name = "fluentDesignFormControl1";
+            fluentDesignFormControl1.Size = new Size(1797, 62);
+            fluentDesignFormControl1.TabIndex = 2;
+            fluentDesignFormControl1.TabStop = false;
+            // 
+            // fluentFormDefaultManager1
+            // 
+            fluentFormDefaultManager1.Form = this;
+            // 
+            // accordionControlElementKategori
+            // 
+            accordionControlElementKategori.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElementAnimsatici, accordionControlElementNotlar });
+            accordionControlElementKategori.Expanded = true;
+            accordionControlElementKategori.Name = "accordionControlElementKategori";
+            accordionControlElementKategori.Text = "Kategori";
+            // 
+            // accordionControlElementAnimsatici
+            // 
+            accordionControlElementAnimsatici.Name = "accordionControlElementAnimsatici";
+            accordionControlElementAnimsatici.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElementAnimsatici.Text = "Anımsatıcı";
+            // 
+            // accordionControlElementNotlar
+            // 
+            accordionControlElementNotlar.Name = "accordionControlElementNotlar";
+            accordionControlElementNotlar.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElementNotlar.Text = "Notlar";
+            // 
+            // accordionControlElementHedefler
+            // 
+            accordionControlElementHedefler.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElementKisaVadeliHedefler, accordionControlElementOrtaVadeHedefeler, accordionControlElementUzunVadeHedefler });
+            accordionControlElementHedefler.Name = "accordionControlElementHedefler";
+            accordionControlElementHedefler.Text = "Hedefler";
+            // 
+            // accordionControlElementKisaVadeliHedefler
+            // 
+            accordionControlElementKisaVadeliHedefler.Name = "accordionControlElementKisaVadeliHedefler";
+            accordionControlElementKisaVadeliHedefler.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElementKisaVadeliHedefler.Text = "Kısa Vadeli Hedefler";
+            // 
+            // accordionControlElementOrtaVadeHedefeler
+            // 
+            accordionControlElementOrtaVadeHedefeler.Name = "accordionControlElementOrtaVadeHedefeler";
+            accordionControlElementOrtaVadeHedefeler.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElementOrtaVadeHedefeler.Text = "Orta Vadeli Hedefler";
+            // 
+            // accordionControlElementUzunVadeHedefler
+            // 
+            accordionControlElementUzunVadeHedefler.Name = "accordionControlElementUzunVadeHedefler";
+            accordionControlElementUzunVadeHedefler.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElementUzunVadeHedefler.Text = "Uzun Vade Hedefler";
+            // 
+            // accordionControlElementHakkımda
+            // 
+            accordionControlElementHakkımda.Name = "accordionControlElementHakkımda";
+            accordionControlElementHakkımda.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElementHakkımda.Text = "Hakkımda";
+            // 
+            // AnaFrm
+            // 
+            Appearance.BackColor = Color.Gray;
+            Appearance.Options.UseBackColor = true;
+            AutoScaleDimensions = new SizeF(12F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1797, 1052);
+            ControlContainer = fluentDesignFormContainer1;
+            Controls.Add(fluentDesignFormContainer1);
+            Controls.Add(accordionControl1);
+            Controls.Add(fluentDesignFormControl1);
+            FluentDesignFormControl = fluentDesignFormControl1;
+            MaximumSize = new Size(1801, 1054);
+            MinimumSize = new Size(1801, 1054);
+            Name = "AnaFrm";
+            NavigationControl = accordionControl1;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AnaFrm";
+            ((System.ComponentModel.ISupportInitialize)accordionControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)fluentDesignFormControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)fluentFormDefaultManager1).EndInit();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer fluentDesignFormContainer1;
+        private DevExpress.XtraBars.Navigation.AccordionControl accordionControl1;
+        private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl fluentDesignFormControl1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement1;
+        private DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager fluentFormDefaultManager1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementKategori;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementAnimsatici;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementNotlar;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementHedefler;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementKisaVadeliHedefler;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementOrtaVadeHedefeler;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementUzunVadeHedefler;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElementHakkımda;
+    }
+}
