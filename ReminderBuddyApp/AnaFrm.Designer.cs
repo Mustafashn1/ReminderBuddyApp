2eaeccb3a8987e885efae2b0cfa0ffa3ae90604e
Musta <Mustafashn.1@outlook.com>
2025-03-03 11:17:58 +0300

    Kategoriler düzenlendi hedefler anımsatıcı notlar eklendi

--- ReminderBuddyApp/AnaFrm.Designer.cs
+++ ReminderBuddyApp/AnaFrm.Designer.cs
@@ -33,11 +33,15 @@
             fluentDesignFormControl1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl();
             fluentFormDefaultManager1 = new DevExpress.XtraBars.FluentDesignSystem.FluentFormDefaultManager(components);
             fluentDesignFormContainer1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer();
+            simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
             gridControl1 = new DevExpress.XtraGrid.GridControl();
             gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
             gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
-            simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            accordionControlElement1 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement2 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement3 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            accordionControlElement4 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
             ((System.ComponentModel.ISupportInitialize)accordionControl1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)fluentDesignFormControl1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)fluentFormDefaultManager1).BeginInit();
@@ -49,10 +53,12 @@
             // accordionControl1
             // 
             accordionControl1.Dock = DockStyle.Left;
-            accordionControl1.Location = new Point(0, 62);
+            accordionControl1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement1 });
+            accordionControl1.Location = new Point(0, 31);
+            accordionControl1.Margin = new Padding(2, 2, 2, 2);
             accordionControl1.Name = "accordionControl1";
             accordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Touch;
-            accordionControl1.Size = new Size(414, 990);
+            accordionControl1.Size = new Size(207, 522);
             accordionControl1.TabIndex = 1;
             accordionControl1.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu;
             // 
@@ -61,8 +67,9 @@
             fluentDesignFormControl1.FluentDesignForm = this;
             fluentDesignFormControl1.Location = new Point(0, 0);
             fluentDesignFormControl1.Manager = fluentFormDefaultManager1;
+            fluentDesignFormControl1.Margin = new Padding(2, 2, 2, 2);
             fluentDesignFormControl1.Name = "fluentDesignFormControl1";
-            fluentDesignFormControl1.Size = new Size(1797, 62);
+            fluentDesignFormControl1.Size = new Size(906, 31);
             fluentDesignFormControl1.TabIndex = 2;
             fluentDesignFormControl1.TabStop = false;
             fluentDesignFormControl1.Click += fluentDesignFormControl1_Click;
@@ -79,18 +86,30 @@
             fluentDesignFormContainer1.Controls.Add(gridControl1);
             fluentDesignFormContainer1.Dock = DockStyle.Fill;
             fluentDesignFormContainer1.Font = new Font("Corbel", 13.875F, FontStyle.Regular, GraphicsUnit.Point, 162);
-            fluentDesignFormContainer1.Location = new Point(414, 62);
+            fluentDesignFormContainer1.Location = new Point(207, 31);
+            fluentDesignFormContainer1.Margin = new Padding(2, 2, 2, 2);
             fluentDesignFormContainer1.Name = "fluentDesignFormContainer1";
-            fluentDesignFormContainer1.Size = new Size(1383, 990);
+            fluentDesignFormContainer1.Size = new Size(699, 522);
             fluentDesignFormContainer1.TabIndex = 0;
+            // 
+            // simpleButton1
+            // 
+            simpleButton1.Location = new Point(34, 11);
+            simpleButton1.Margin = new Padding(2, 3, 2, 3);
+            simpleButton1.Name = "simpleButton1";
+            simpleButton1.Size = new Size(80, 33);
+            simpleButton1.TabIndex = 1;
+            simpleButton1.Text = "simpleButton1";
             // 
             // gridControl1
             // 
-            gridControl1.Location = new Point(67, 104);
+            gridControl1.EmbeddedNavigator.Margin = new Padding(2, 2, 2, 2);
+            gridControl1.Location = new Point(34, 54);
             gridControl1.MainView = gridView1;
+            gridControl1.Margin = new Padding(2, 2, 2, 2);
             gridControl1.MenuManager = fluentFormDefaultManager1;
             gridControl1.Name = "gridControl1";
-            gridControl1.Size = new Size(1071, 775);
+            gridControl1.Size = new Size(536, 403);
             gridControl1.TabIndex = 0;
             gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] { gridView1 });
             gridControl1.Click += gridControl1_Click;
@@ -98,50 +117,67 @@
             // gridView1
             // 
             gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] { gridColumn1, gridColumn2 });
+            gridView1.DetailHeight = 182;
             gridView1.GridControl = gridControl1;
+            gridView1.LevelIndent = 0;
             gridView1.Name = "gridView1";
+            gridView1.OptionsEditForm.PopupEditFormWidth = 400;
+            gridView1.PreviewIndent = 0;
             // 
             // gridColumn1
             // 
             gridColumn1.Caption = "Hedef";
-            gridColumn1.MinWidth = 40;
             gridColumn1.Name = "gridColumn1";
             gridColumn1.Visible = true;
             gridColumn1.VisibleIndex = 0;
-            gridColumn1.Width = 150;
             // 
             // gridColumn2
             // 
             gridColumn2.Caption = "Saat";
-            gridColumn2.MinWidth = 40;
             gridColumn2.Name = "gridColumn2";
             gridColumn2.Visible = true;
             gridColumn2.VisibleIndex = 1;
-            gridColumn2.Width = 150;
             // 
-            // simpleButton1
+            // accordionControlElement1
             // 
-            simpleButton1.Location = new Point(67, 22);
-            simpleButton1.Margin = new Padding(5);
-            simpleButton1.Name = "simpleButton1";
-            simpleButton1.Size = new Size(159, 64);
-            simpleButton1.TabIndex = 1;
-            simpleButton1.Text = "simpleButton1";
+            accordionControlElement1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] { accordionControlElement2, accordionControlElement3, accordionControlElement4 });
+            accordionControlElement1.Expanded = true;
+            accordionControlElement1.Name = "accordionControlElement1";
+            accordionControlElement1.Text = "Kategoriler";
+            // 
+            // accordionControlElement2
+            // 
+            accordionControlElement2.Name = "accordionControlElement2";
+            accordionControlElement2.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement2.Text = "Hedefler";
+            // 
+            // accordionControlElement3
+            // 
+            accordionControlElement3.Name = "accordionControlElement3";
+            accordionControlElement3.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement3.Text = "Anımsatıcı";
+            // 
+            // accordionControlElement4
+            // 
+            accordionControlElement4.Name = "accordionControlElement4";
+            accordionControlElement4.Style = DevExpress.XtraBars.Navigation.ElementStyle.Item;
+            accordionControlElement4.Text = "Notlar";
             // 
             // AnaFrm
             // 
             Appearance.BackColor = Color.Gray;
             Appearance.Options.UseBackColor = true;
-            AutoScaleDimensions = new SizeF(12F, 25F);
+            AutoScaleDimensions = new SizeF(6F, 13F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1797, 1052);
+            ClientSize = new Size(906, 553);
             ControlContainer = fluentDesignFormContainer1;
             Controls.Add(fluentDesignFormContainer1);
             Controls.Add(accordionControl1);
             Controls.Add(fluentDesignFormControl1);
             FluentDesignFormControl = fluentDesignFormControl1;
+            Margin = new Padding(2, 2, 2, 2);
             MaximumSize = new Size(1801, 1054);
-            MinimumSize = new Size(1801, 1054);
+            MinimumSize = new Size(1799, 1030);
             Name = "AnaFrm";
             NavigationControl = accordionControl1;
             StartPosition = FormStartPosition.CenterScreen;
@@ -166,5 +202,9 @@
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
         private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement2;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement3;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement4;
     }
 }
